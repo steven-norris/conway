a251792a67c67af8caf19102e6ba354f98a98918
norri <norris.stevenj@gmail.com>
2024-10-23 14:33:06 -0700

    Add project files.

---
+++ ConwayLife/LifeState.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConwayLife
+{
+    public class LifeState
+    {
+        public LifeState(int LifeRows, int LifeColumns)
+        {
+            Rows = LifeRows;
+            Columns = LifeColumns;
+            Values = new bool[Rows,Columns];
+            Randomize();
+        }
+
+        public int Rows
+        {
+            get;
+            private set;
+        }
+        public int Columns
+        {
+            get;
+            private set;
+        }
+        public bool[,] Values
+        {
+            get;
+            private set;
+        }
+
+        public void Randomize()
+        {
+            Random Randomizer = new Random(Environment.TickCount);
+
+            for(int RowCounter = 0; RowCounter < Rows; RowCounter++)
+            {
+                for(int ColumnCounter = 0; ColumnCounter < Columns; ColumnCounter++)
+                {
+                    switch(Randomizer.Next(2))
+                    {
+                        case 1:
+                            Values[RowCounter,ColumnCounter] = true;
+                            break;
+                        default:
+                            Values[RowCounter, ColumnCounter] = false;
+                            break;
+                    }
+                }
+            }
+        }
+
+        public void Advance()
+        {
+            /*Rules Of Life
+            All squares directly orthogonal or diagonal from another square count as neighbors.
+            
+            1.Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+            2.Any live cell with two or three live neighbours lives on to the next generation.
+            3.Any live cell with more than three live neighbours dies, as if by overcrowding.
+            4.Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+            */
+
+            //Remove the line below and implement as described above.
+            Randomize();
+        }
+    }
+}
---
+++ ConwayLife/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Timers;
+
+namespace ConwayLife
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            RunState = new LifeState(40, 40);
+            InitializeGrid(RunState);
+
+            LifeGridUpdateTimer = new Timer(500);
+            LifeGridUpdateTimer.Elapsed += LifeGridUpdate;
+            LifeGridUpdateTimer.Start();
+        }
+
+        public LifeState RunState
+        {
+            get;
+            private set;
+        }
+        public bool IsUpdating
+        {
+            get;
+            set;
+        }
+
+        public void InitializeGrid(LifeState state)
+        {
+            LifeGrid.RowDefinitions.Clear();
+            LifeGrid.ColumnDefinitions.Clear();
+
+            for(int RowCount = 0; RowCount < state.Rows; RowCount++)
+            {
+                LifeGrid.RowDefinitions.Add(new RowDefinition());
+            }
+            for(int ColumnCount = 0; ColumnCount < state.Columns; ColumnCount++)
+            {
+                LifeGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+
+            for(int RowCount = 0; RowCount < state.Rows; RowCount++)
+            {
+                for(int ColumnCount = 0; ColumnCount < state.Columns; ColumnCount++)
+                {
+                    Canvas LifeElement = new Canvas();
+                    Grid.SetRow(LifeElement, RowCount);
+                    Grid.SetColumn(LifeElement, ColumnCount);
+                    LifeGrid.Children.Add(LifeElement);
+                }
+            }
+        }
+
+        void LifeGridUpdate(object sender, ElapsedEventArgs e)
+        {
+            if (IsUpdating == false)
+            {
+                IsUpdating = true;
+
+                App.Current.Dispatcher.Invoke((Action)delegate
+                {
+                    foreach (Canvas LifeElement in LifeGrid.Children)
+                    {
+                        if (RunState.Values[Grid.GetRow(LifeElement), Grid.GetColumn(LifeElement)] == true)
+                        {
+                            LifeElement.Background = Brushes.Black;
+                        }
+                        else
+                        {
+                            LifeElement.Background = Brushes.AntiqueWhite;
+                        }
+                    }
+                });
+
+                RunState.Advance();
+
+                IsUpdating = false;
+            }
+        }
+
+        private Timer LifeGridUpdateTimer;
+    }
+}
